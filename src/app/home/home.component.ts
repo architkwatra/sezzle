69515674f2692b153408025cbc2222444959f583
archit kwatra <qrchit.kwatra@gmail.com>
2021-02-16 20:13:44 -0500

    in a good shape

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, HostListener, OnInit, SkipSelf } from '@angular/core';
 import { BROWSER_STORAGE, BrowserStorageService } from '../browser-storage.service';
-import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-home',
@@ -11,37 +10,35 @@ import { GlobalService } from '../global.service';
 })
 export class HomeComponent implements OnInit {
 
-  constructor( @SkipSelf() private localStorageService: BrowserStorageService, private globalService: GlobalService) { }
+  constructor( @SkipSelf() private localStorageService: BrowserStorageService) { }
 
   @HostListener('window: storage', ['$event'])
   localStorageChange(event: Storage) {
-    console.log("WWWWWWWWWWWWWWWWWWWWWWWWWW", event);
     this.updateLocalStorage();
+  }
 
-  } 
-
-  currentExpression = ""
-  consolidate = "0";
-  content = [];
-  expressionStack = [];
-  operator = "";
-  shouldSetCalc = false;
-  num = "";
-  calcStack = []
-  limit = 11;
-  showAlert = false;
-  results: any =  [];
+  currentExpression: string = ""
+  consolidate: string = "";
+  content = new Array();
+  expressionStack = new Array();
+  operator: string = "";
+  shouldSetCalc: boolean = false;
+  num: string = "";
+  calcStack = new Array();
+  limit: number = 10;
+  characterLimit: number = 20;
+  showAlert: boolean = false;
+  results: any = new Array();
   
   ngOnInit(): void {
-    this.content = [["1","2","3","+"],["4","5","6","-"],["7","8","9","/"],["*","0","#","X"], ["C", "<-", "=", "."]];
-    // this.localStorageService.set('results', "");
-    this.results = this.localStorageService.get("results").split(",");
-  
+    this.content = [["1","2","3","+"],["4","5","6","-"],["7","8","9","/"],["*","0","#","X"], ["C", "00", "=", "."]];
+    if (this.checkLocalStorage()) {
+      this.results = this.localStorageService.get("results").split(",");  
+    }
   }
+
   handleClick(value: string) {
-
-    if (this.expressionStack.length + this.num.length < 11 || value == "C") {
-
+    if (this.expressionStack.length + this.num.length < this.characterLimit || value == "C") {
       if (this.shouldSetCalc) {
         this.expressionStack = [];
         this.consolidate = "";
@@ -55,20 +52,20 @@ export class HomeComponent implements OnInit {
           this.expressionStack.push(value);
         }       
       } else if (value == "C") {
-        this.expressionStack = [];
-        this.showAlert = false;
-        this.num = "0";
+          this.expressionStack = [];
+          this.showAlert = false;
+          this.num = "";
       } else if (value === "=") {
-        if (this.num) 
-          this.expressionStack.push(this.num);
-        this.num = ""
-        this.shouldSetCalc = true;
-        this.calculate(this.expressionStack);
-        return;
+          if (this.num)  this.expressionStack.push(this.num);
+            this.num = ""
+            this.shouldSetCalc = true;
+            this.calculate(this.expressionStack);
+            return;
       } else {
-        this.num += value;
+        if ((value == "." && !isNaN(parseFloat(this.num[this.num.length - 1])) || !isNaN(parseFloat(value)))) 
+          this.num += value;
       }
-      this.consolidate = this.expressionStack.join(" ") + this.num;
+      this.consolidate = this.expressionStack.join(" ") + " " + this.num;
       this.currentExpression = this.consolidate;
     } else {
       this.showAlert = true;
@@ -76,16 +73,13 @@ export class HomeComponent implements OnInit {
   }
 
   calculate(expressionStack) {
-    let num1 = 0,i = 0;
-    let sign = "+";
-    
+    let num1 = 0, sign = "+";
     this.results = "";
     for (let i = 0; i < expressionStack.length; i++) {
       let n = expressionStack[i];
       if (!isNaN(parseFloat(n)))
         num1 = parseFloat(n);
       if (isNaN(parseFloat(n)) || (i == this.expressionStack.length - 1)) {
-      
         if (sign == "+") 
           this.calcStack.push(num1);
         else if (sign == "-")
@@ -94,40 +88,55 @@ export class HomeComponent implements OnInit {
           this.calcStack.push((parseFloat(this.calcStack.pop()) / num1));
         else if (sign == "X")
           this.calcStack.push(num1 * (parseFloat(this.calcStack.pop())));
-
-          num1 = 0;
-          sign = n;
+        num1 = 0;
+        sign = n;
       }
     }
+    this.postEvaluation();
+  }
 
+  updateLocalStorage() {
+
+    if (this.checkLocalStorage()) {
+      this.results = this.localStorageService.get('results').split(",");
+    }
+    
+    if (this.results.length == this.limit && this.currentExpression != "") {
+      this.results = this.results.splice(1, this.results.length);
+    }
+
+    if (this.currentExpression != "") {
+      this.results.push(this.currentExpression);
+    }
+    
+    let temp = this.results.join(",");
+    if (temp[0] == ",") {
+      temp = temp.splice(1, temp.length - 1);
+    }
+    this.localStorageService.set('results', temp);
+    this.currentExpression = "";
+  }
+
+  postEvaluation() {
     this.consolidate = this.calcStack.reduce(function (a, b) {
       return a + b;
     }, 0);
     this.calcStack = [];
     this.expressionStack = [];
-    this.currentExpression += " = " + this.consolidate;
-    
+    this.currentExpression += " = " + this.consolidate;    
     this.updateLocalStorage();
   }
-
-  updateLocalStorage() {
-
-    this.results = this.localStorageService.get('results').split(",");
-    
-    if (this.results.length > 9) {
-      this.results = this.results.splice(this.results.length-9, this.results.length-1);
-    }
-
-    if (this.currentExpression)
-      this.results.push(this.currentExpression);
-    this.localStorageService.set('results', this.results.join(","));
-    this.currentExpression = "";
-    
-  }
+  
   closeAlert() {
     this.showAlert = false;
     this.expressionStack = [];
     this.showAlert = false;
     this.num = "0";
   }
+  checkLocalStorage() {
+    if (this.localStorageService.get('results') != null && this.localStorageService.get('results') != undefined)
+      return true;
+    return false;
+  }
 }
+
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,31 +1,16 @@
 import { Component, Input, OnInit, SkipSelf } from '@angular/core';
-import { BROWSER_STORAGE, BrowserStorageService } from '../browser-storage.service';
-import { GlobalService } from '../global.service';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss'],
-  providers: [ BrowserStorageService,
-    { provide: BROWSER_STORAGE, useFactory: () => sessionStorage }]
+  styleUrls: ['./results.component.scss']
 })
 export class ResultsComponent implements OnInit {
 
   @Input() results: string[];
 
-  constructor(@SkipSelf() private localStorageService: BrowserStorageService, private globalService: GlobalService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.updateData();
   }
-
-  updateData() {
-    this.globalService.checkLocalStorageUpdate.subscribe( data =>  {
-      if (data)
-        this.results = this.localStorageService.get('results').split(",");
-      console.log(this.results);
-    });
-
-  }
-
 }
